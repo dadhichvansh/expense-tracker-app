30a05cbbed9de0cb9e86ec11bd72bee374ba973a
vansh-home <dadhichvansh46@gmail.com>
2025-01-31 17:07:18 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
-import './style.css';
+import "./style.css";
+
+const form = document.querySelector("#form");
+const expenseList = document.querySelector("#expense-list");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const description = document.querySelector("#description").value;
+  const category = document.querySelector("#category").value;
+  const amount = document.querySelector("#amount").value;
+
+  if (description && category && !isNaN(amount)) {
+    const item = document.createElement("tr");
+    item.innerHTML = `
+      <td>${description}</td>
+      <td>${category}</td>
+      <td>${amount}</td>
+    `;
+    expenseList.appendChild(item);
+
+    document.querySelector("#description").value = "";
+    document.querySelector("#category").value = "";
+    document.querySelector("#amount").value = "";
+  } else {
+    alert("Please fill out all the fields correctly");
+  }
+});
